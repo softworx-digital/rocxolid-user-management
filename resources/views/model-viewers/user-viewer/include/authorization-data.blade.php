240fb9aeab5879901f2b57ad502a1ef37d982ae4
Peter Bolemant <peter.bolemant@softworx.digital>
2020-04-16 15:10:37 +0200

    [in progress] refactoring

--- resources/views/model-viewers/user-viewer/include/authorization-data.blade.php
+++ resources/views/model-viewers/user-viewer/include/authorization-data.blade.php
@@ -10,19 +10,19 @@
     </div>
     <div class="panel-body">
         <dl class="dl-horizontal">
-        @foreach ($component->getModel()->getRelationshipMethods() as $method)
-        @can ('assign', [ $component->getModel(), $method ])
-            <dt>{{ $component->translate(sprintf('field.%s', $method)) }}</dt>
-            <dd>
-            @foreach ($component->getModel()->$method()->get() as $item)
-                @can ('update', $item)
-                    <a class="label label-info" data-ajax-url="{{ $item->getControllerRoute() }}">{{ $item->getTitle() }}</a>
-                @elsecan('view', $item)
-                    <span class="label label-info">{{ $item->getTitle() }}</span>
-                @endcan
-            @endforeach
-            </dd>
-        @endcan
+        @foreach ($component->getModel()->getRelationshipMethods() as $attribute)
+            @can ('assign', [ $component->getModel(), $attribute ])
+                <dt>{{ $component->translate(sprintf('field.%s', $attribute)) }}</dt>
+                <dd>
+                @foreach ($component->getModel()->$attribute()->get() as $item)
+                    @can ('update', $item)
+                        <a class="label label-info" data-ajax-url="{{ $item->getControllerRoute() }}">{{ $item->getTitle() }}</a>
+                    @elsecan('view', $item)
+                        <span class="label label-info">{{ $item->getTitle() }}</span>
+                    @endcan
+                @endforeach
+                </dd>
+            @endcan
         @endforeach
         </dl>
     </div>

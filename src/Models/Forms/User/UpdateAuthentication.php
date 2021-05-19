c195170f3dd8bfc6e0db3d9fa66e63a96dd0cf37
Peter Bolemant <peter@softworx.digital>
2021-05-19 20:35:03 +0000

    general update

--- src/Models/Forms/User/UpdateAuthentication.php
+++ src/Models/Forms/User/UpdateAuthentication.php
@@ -2,8 +2,11 @@
 
 namespace Softworx\RocXolid\UserManagement\Models\Forms\User;
 
+use Illuminate\Support\Collection;
+// rocXolid forms
 use Softworx\RocXolid\Forms\AbstractCrudForm as RocXolidAbstractCrudForm;
-use Softworx\RocXolid\Forms\Fields\Type\Password;
+// rocXolid form field types
+use Softworx\RocXolid\Forms\Fields\Type as FieldType;
 
 class UpdateAuthentication extends RocXolidAbstractCrudForm
 {
@@ -15,15 +18,31 @@ class UpdateAuthentication extends RocXolidAbstractCrudForm
     ];
 
     protected $fields = [
+        'email' => [
+            'type' => FieldType\Email::class,
+            'options' => [
+                'label' => [
+                    'title' => 'email',
+                ],
+                'validation' => [
+                    'rules' => [
+                        'required',
+                        'email',
+                        // 'unique:users,email',
+                    ],
+                ],
+            ],
+        ],
         'password' => [
-            'type' => Password::class,
+            'type' => FieldType\Password::class,
             'options' => [
                 'label' => [
                     'title' => 'password'
                 ],
                 'validation' => [
                     'rules' => [
-                        'required',
+                        // 'required',
+                        'nullable',
                         'max:255',
                         'min:6',
                         //'regex:/^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\X])(?=.*[!$#%]).*$/',
@@ -33,7 +52,7 @@ class UpdateAuthentication extends RocXolidAbstractCrudForm
             ],
         ],
         'password_confirmation' => [
-            'type' => Password::class,
+            'type' => FieldType\Password::class,
             'options' => [
                 'label' => [
                     'title' => 'password_confirmation'
@@ -41,4 +60,33 @@ class UpdateAuthentication extends RocXolidAbstractCrudForm
             ],
         ],
     ];
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function adjustFieldsDefinition($fields)
+    {
+        $user = auth('rocXolid')->user();
+
+        if ($user && $user->can('update', $this->getModel()) && $user->can('viewAnyAll', $this->getModel())) {
+            $fields['email']['options']['validation']['rules'][] = sprintf('unique:users,email,%s', $this->getModel()->getKey());
+        } else {
+            unset($fields['email']);
+
+            $fields['password']['options']['validation']['rules'][] = 'required';
+        }
+
+        return $fields;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getFormFieldsValues(): Collection
+    {
+        $values = parent::getFormFieldsValues();
+        $values = $values->filter();
+
+        return $values;
+    }
 }
